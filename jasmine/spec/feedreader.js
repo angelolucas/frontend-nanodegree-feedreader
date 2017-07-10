c1408b0958dc9131757b41dc9bcfbc11e73e9ea0
Ângelo Lucas <angelo.lrc@gmail.com>
2017-07-10 15:43:01 -0300

    test: has defined URLs

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -26,12 +26,16 @@ $(function() {
       expect(allFeeds.length).not.toBe(0);
     });
 
-
-    /* TODO: Write a test that loops through each feed
+    /* Write a test that loops through each feed
     * in the allFeeds object and ensures it has a URL defined
     * and that the URL is not empty.
     */
-
+    it('has defined URLs', function(){
+      allFeeds.forEach(function(feed) {
+        expect(feed.url).toBeDefined();
+        expect(feed.url).not.toBe('');
+      });
+    });
 
     /* TODO: Write a test that loops through each feed
     * in the allFeeds object and ensures it has a name defined
